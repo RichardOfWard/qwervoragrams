1e37e0807672e3f1a6810017a79f640f614b26a3
Richard Ward <richard@richard.ward.name>
2015-10-13 22:50:31 +0100

    add output

--- src/App.scala
+++ src/App.scala
@@ -1,9 +1,12 @@
+import java.io.PrintWriter
+
 import scala.io.Source
 
 object App {
   def main(args: Array[String]) = {
     val words = Source.fromFile("res/words.txt").getLines().map(_.trim).toSet
     val finder = new QwervoragramFinder(words)
-    finder.find.foreach(println)
+    val output = new PrintWriter("output/qwerverograms.txt")
+    finder.find.foreach(output.println)
   }
 }
